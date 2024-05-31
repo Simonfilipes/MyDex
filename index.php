5d42c50c5992036149f4cf59caa9e19ea8b5a377
Simon Filipe <167369596+Simonfilipes@users.noreply.github.com>
2024-05-31 12:38:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/select.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/box.css">
+    <title>MyDex</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <heaader>
+        <section class="section_header">
+            <div class="logo">
+                <img src="img/logo.jpg" alt="Logo" id="id_logo">
+            </div>
+        </section>
+    </heaader>
+    <!-- HEADER -->
+
+    <!-- PARTE SUPERIOR GEN -->
+    <div class="radio-input">
+    <input value="value-0" name="value-radio" id="value-0" type="radio">
+        <label for="value-0">All Pokemon</label>
+        <input value="value-1" name="value-radio" id="value-1" type="radio">
+        <label for="value-1">1 Geração</label>
+        <input value="value-2" name="value-radio" id="value-2" type="radio">
+        <label for="value-2">2 Geração</label>
+        <input value="value-3" name="value-radio" id="value-3" type="radio">
+        <label for="value-3">3 Geração</label>
+        <input value="value-4" name="value-radio" id="value-4" type="radio">
+        <label for="value-4">4 Geração</label>
+        <input value="value-5" name="value-radio" id="value-5" type="radio">
+        <label for="value-5">5 Geração</label>
+        <input value="value-6" name="value-radio" id="value-6" type="radio">
+        <label for="value-6">3 Geração</label>
+        <input value="value-7" name="value-radio" id="value-7" type="radio">
+        <label for="value-7">7 Geração</label>
+        <input value="value-8" name="value-radio" id="value-8" type="radio">
+    </div>
+    <!-- PARTE SUPERIOR GEN -->
+
+    <!-- PARTE PRINCIPAL -->
+
+    <section class="box">
+
+    </section>
+
+    <!-- PARTE PRINCIPAL -->
+</body>
+
+</html>
